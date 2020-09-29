a3c3a3ed86e3fba31c8ee32804f73230d8ca84fb
kwmorlock <kmorlockcodes@gmail.com>
2020-09-29 19:26:38 -0400

    chore: App.js code cleanup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,20 +2,18 @@ import React from "react";
 import "./App.css";
 import { Route } from "react-router-dom";
 import Home from "./Home/Home";
-import Contact from "./Contact/Contact"
+import Contact from "./Contact/Contact";
 
 function App() {
   return (
-<div>
-<Route exact path="/">
+    <div>
+      <Route exact path="/">
         <Home />
       </Route>
       <Route exact path="/contact">
         <Contact />
       </Route>
-</div>
-
-    
+    </div>
   );
 }
 
