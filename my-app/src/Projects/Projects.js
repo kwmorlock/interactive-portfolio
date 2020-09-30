428a1b2d2ad8ddbd0f342e06329458515a23b745
kwmorlock <kmorlockcodes@gmail.com>
2020-09-30 00:21:54 -0400

    feat: Projects.js created

---
+++ my-app/src/Projects/Projects.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../Nav/Nav"
+
+const Projects = () => {
+  return (
+    <div>
+        <Nav />
+      <header>
+        <p>Projects</p>
+      </header>
+    </div>
+  );
+}
+
+export default Projects;
