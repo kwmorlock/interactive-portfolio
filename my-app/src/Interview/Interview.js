f9ead73b55887c6c54e8c0d770434d8b3aa8ed39
kwmorlock <kmorlockcodes@gmail.com>
2020-09-30 00:39:58 -0400

    feat: Interview.js created

---
+++ my-app/src/Interview/Interview.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../Nav/Nav"
+
+const Interview = () => {
+  return (
+    <div>
+        <Nav />
+      <header>
+        <p>Interview</p>
+      </header>
+    </div>
+  );
+}
+
+export default Interview;
