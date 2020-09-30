34c3cf362ba19a4e787399d88a419db71739fdcb
kwmorlock <kmorlockcodes@gmail.com>
2020-09-29 20:47:45 -0400

    chore: nav.js link styling

--- my-app/src/Nav/Nav.js
+++ my-app/src/Nav/Nav.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./NavStyles";
 
 const Nav = () => {
-    return (
-      <div>
-        <header>
-          <p>Nav</p>
-        </header>
-      </div>
-    );
-  }
-  
-  export default Nav;
+  return (
+    <div>
+      <Link
+        to={"/"}
+        style={{
+          color: "hotpink",
+          textDecoration: "none",
+          textAlign: "center",
+        }}
+      >
+        {" "}
+        Home{" "}
+      </Link>
+
+      <Link
+        to={"/contact"}
+        style={{
+          color: "hotpink",
+          textDecoration: "none",
+          textAlign: "center",
+        }}
+      >
+        {" "}
+        Contact{" "}
+      </Link>
+      <header>
+        <p>Nav</p>
+      </header>
+    </div>
+  );
+};
+
+export default Nav;
