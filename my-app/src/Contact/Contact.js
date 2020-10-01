b1e464a5670660a136023ca5932f48f363969ac8
kwmorlock <kmorlockcodes@gmail.com>
2020-10-01 17:53:14 -0400

    chore: Contact.js code cleanup

--- my-app/src/Contact/Contact.js
+++ my-app/src/Contact/Contact.js
@@ -2,27 +2,34 @@ import React from "react";
 import { HomeWrapper } from "../Nav/NavStyles";
 import Nav from "../Nav/Nav";
 import { ContactLeft, FlexContact, FlexContactTwo } from "./ContactStyles";
-import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
     <HomeWrapper>
-    <Nav />
-    <FlexContact>
-      <ContactLeft>
-        <h2 class="mysite"> GitHub</h2>
-        <p>
-          {" "}
-          Connect on github to check out some of my clean and readable code.
-        </p>
-        <button class="bodybutton" onclick="window.location.href = 'https://github.com/kwmorlock'"> <a target="_blank" href='https://github.com/kwmorlock'>My GitHub!</a></button> 
-      </ContactLeft>
-      <ContactLeft>
-        <img src={require("../Images/readablecode.png")} />
-      </ContactLeft>
-    </FlexContact>
+      <Nav />
+      <FlexContact>
+        <ContactLeft>
+          <h2 class="mysite"> GitHub</h2>
+          <p>
+            {" "}
+            Connect on github to check out some of my clean and readable code.
+          </p>
+          <button
+            class="bodybutton"
+            onclick="window.location.href = 'https://github.com/kwmorlock'"
+          >
+            {" "}
+            <a target="_blank" href="https://github.com/kwmorlock">
+              My GitHub!
+            </a>
+          </button>
+        </ContactLeft>
+        <ContactLeft>
+          <img src={require("../Images/readablecode.png")} />
+        </ContactLeft>
+      </FlexContact>
     </HomeWrapper>
   );
-}
+};
 
 export default Contact;
