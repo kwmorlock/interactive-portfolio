412237cdcc8e4cf825cf2b0ef599d54179c6fa28
kwmorlock <kmorlockcodes@gmail.com>
2020-10-01 19:12:28 -0400

    feat: Contact.js Twitter section added

--- my-app/src/Contact/Contact.js
+++ my-app/src/Contact/Contact.js
@@ -28,6 +28,28 @@ const Contact = () => {
           <img src={require("../Images/readablecode.png")} />
         </ContactLeft>
       </FlexContact>
+      <FlexContactTwo>
+        <ContactLeft>
+          <img src={require("../Images/pinklaptop.jpeg")} />
+        </ContactLeft>
+        <ContactLeft>
+          <h2 class="mysite"> Twitter </h2>
+          <p>
+            {" "}
+            Check out some of my posts on Twitter relating to coding and tech
+            for both the present and the potential future of both.
+          </p>
+          <button
+            class="bodybutton"
+            onclick="window.location.href = 'https://twitter.com/KMCodes'"
+          >
+            {" "}
+            <a target="_blank" href="https://twitter.com/KMCodes">
+              My Twitter!
+            </a>
+          </button>
+        </ContactLeft>
+      </FlexContactTwo>
     </HomeWrapper>
   );
 };
