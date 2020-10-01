f559fe56b7126e075f7a87dde59555a376dc5d33
kwmorlock <kmorlockcodes@gmail.com>
2020-10-01 01:08:55 -0400

    chore: Home structure updated

--- my-app/src/Home/Home.js
+++ my-app/src/Home/Home.js
@@ -37,6 +37,21 @@ const Home = () => {
           </p>
         </HomeLeft>
       </FlexHomeTwo>
+      <FlexHome>
+        <HomeLeft>
+          <h2 class="mysite"> Expert Programmer </h2>
+          <p>
+            {" "}
+            Quite possibly one of the best coders in all the land. If you like
+            dollars, I can make you many dollars. If you want a pretty website,
+            I can make you a super pretty website. To top it all off check out
+            these eyes they can find any syntax error.
+          </p>
+        </HomeLeft>
+        <HomeLeft>
+          <img src={require("../Images/pinkhairs.jpg")} />
+        </HomeLeft>
+      </FlexHome>
     </HomeWrapper>
   );
 };
