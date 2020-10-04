c243047701dfbc2a5c7f0bdb3a51965a7b4d5e9b
kwmorlock <kmorlockcodes@gmail.com>
2020-10-04 02:20:30 -0400

    chore: Projects.js Project1 setup

--- my-app/src/Projects/Projects.js
+++ my-app/src/Projects/Projects.js
@@ -2,12 +2,11 @@ import React from "react";
 import { HomeWrapper } from "../Nav/NavStyles";
 import Nav from "../Nav/Nav";
 import {
-  Contactprojects,
+  ProjectsLeft,
   FlexProjects,
   FlexProjectsTwo,
   ButtonProjects,
 } from "./ProjectsStyles";
-
 
 const Projects = () => {
   function changeColor(e) {
@@ -17,12 +16,34 @@ const Projects = () => {
     e.target.style.color = "hotpink";
   }
   return (
-    <div>
+    <HomeWrapper>
       <Nav />
-      <header>
-        <p>Projects</p>
-      </header>
-    </div>
+      <FlexProjects>
+        <ProjectsLeft>
+          <h2 class="mysite"> Project 1</h2>
+          <p>
+            {" "}
+            Quite possibly one of the best coders in all the land. If you like
+            dollars, I can make you many dollars. If you want a pretty website,
+            I can make you a super pretty website. To top it all off check out
+            these eyes they can find any syntax error.
+          </p>
+          <ButtonProjects
+            class="bodybutton"
+            onclick="_blank"
+            href="https://github.com/kwmorlock"
+            onMouseOver={changeColor}
+            onMouseLeave={changeColors}
+          >
+            {" "}
+            Project 1
+          </ButtonProjects>
+        </ProjectsLeft>
+        <ProjectsLeft>
+          <img src={require("../Images/readablecode.png")} />
+        </ProjectsLeft>
+      </FlexProjects>
+    </HomeWrapper>
   );
 };
 
