d6bdbb8809a560d86a8751b4a0262ab7ffe88137
kwmorlock <kmorlockcodes@gmail.com>
2020-10-04 01:49:19 -0400

    chore: ProjectsStyles.js created

---
+++ my-app/src/Projects/ProjectsStyles.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const ProjectsLeft = styled.div`
+  width: 48%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 4% 7px;
+`;
+
+export const FlexProjects = styled.div`
+  display: flex;
+  flex-direction: row;
+  @media (max-width: 765px) {
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const FlexProjectsTwo = styled.div`
+  display: flex;
+  flex-direction: row;
+  @media (max-width: 765px) {
+    width: 100%;
+    flex-direction: column-reverse;
+    align-items: center;
+  }
+`;
+
+export const ButtonProjects = styled.a`
+  background-color: black;
+  border-color: black;
+  color: hotpink;
+  font-color: hotpink;
+  width: 65%;
+  border-radius: 12px;
+  font-size: 2rem;
+  margin: 0 auto;
+  text-decoration: none;
+  height: 10%;
+  margin-top: 20px;
+  text-align: center;
+`;
