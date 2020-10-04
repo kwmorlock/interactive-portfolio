bd8d1db3ed8f45d36781e1b1dbecded98f6368df
kwmorlock <kmorlockcodes@gmail.com>
2020-10-04 00:54:04 -0400

    feat: Interview video setup

--- my-app/src/Interview/Interview.js
+++ my-app/src/Interview/Interview.js
@@ -8,6 +8,30 @@ const Interview = () => {
       <header>
         <p>Interview</p>
       </header>
+      <div
+        className="video"
+        style={{
+          position: "relative",
+          paddingBottom: "56.25%" /* 16:9 */,
+          paddingTop: 25,
+          height: 0,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <iframe
+          style={{
+            position: "absolute",
+            display: "flex",
+            justifyContent: "center",
+            width: "80%",
+            height: "80%",
+          }}
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          frameBorder="0"
+        />
+      </div>
+      ;
     </div>
   );
 };
