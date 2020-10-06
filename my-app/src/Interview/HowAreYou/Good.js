b2844c3f85a8fbc006e1463b7d6094662631ef79
kwmorlock <kmorlockcodes@gmail.com>
2020-10-06 17:39:00 -0400

    chore: Good.js third link setup

--- my-app/src/Interview/HowAreYou/Good.js
+++ my-app/src/Interview/HowAreYou/Good.js
@@ -44,6 +44,22 @@ const Good = () => {
         {" "}
         Option 2{" "}
       </Link>
+      <Link
+        to={"/"}
+        style={{
+          color: "black",
+          fontColor: "black",
+          width: "65%",
+          borderRadius: "12px",
+          fontSize: "2rem",
+          textDecoration: "none",
+        }}
+        onMouseOver={changeColor}
+        onMouseLeave={changeColors}
+      >
+        {" "}
+        Option 3{" "}
+      </Link>
       <div
         className="video"
         style={{
