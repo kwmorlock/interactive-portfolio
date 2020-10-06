a26fb925823eb81a9cf1c6b0a1395df7c2f6fb4d
kwmorlock <kmorlockcodes@gmail.com>
2020-10-06 15:58:45 -0400

    chore: InterviewContents.div made

--- my-app/src/Interview/InterviewStyles.js
+++ my-app/src/Interview/InterviewStyles.js
@@ -1,1 +1,14 @@
 import styled from "styled-components";
+
+export const InterviewContents = styled.div`
+  margin: 0 auto;
+  padding: 2.2rem;
+  display: flex;
+  width: 66.6%;
+  align-items: center;
+  justify-content: space-between;
+  @media (max-width: 765px) {
+    width: 100%;
+    flex-direction: column;
+  }
+`;
