6fbaec21e9ad4ea83711e66c119684c2e6cb7e07
kwmorlock <kmorlockcodes@gmail.com>
2020-10-10 10:58:07 -0400

    chore: NothingTwo.js Nav setup

--- my-app/src/Interview/HowAreYou/NothingTwo/NothingTwo.js
+++ my-app/src/Interview/HowAreYou/NothingTwo/NothingTwo.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Nav from "../../Nav/Nav";
 
 const NothingTwo = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
 };
 
 export default NothingTwo;
