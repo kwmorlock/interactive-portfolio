ec12838022d30cc0efecdb9df5ad561679c74492
kwmorlock <kmorlockcodes@gmail.com>
2020-10-11 23:24:17 -0400

    chore: Lagging.js Nav added

--- my-app/src/Interview/HowAreYou/NothingTwo/Lagging.js
+++ my-app/src/Interview/HowAreYou/NothingTwo/Lagging.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Nav from "../../../Nav/Nav";
 
 const Lagging = () => {
-  return <div> </div>;
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
 };
 
 export default Lagging;
