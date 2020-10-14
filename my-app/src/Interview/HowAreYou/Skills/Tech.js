c32d9fe91b882d4f8514c4efbbfca51d9a7fc800
kwmorlock <kmorlockcodes@gmail.com>
2020-10-14 01:43:35 -0400

    Tech.js video setup

--- my-app/src/Interview/HowAreYou/Skills/Tech.js
+++ my-app/src/Interview/HowAreYou/Skills/Tech.js
@@ -5,6 +5,29 @@ const Tech = () => {
   return (
     <div>
       <Nav />
+      <div
+        className="video"
+        style={{
+          position: "relative",
+          paddingBottom: "56.25%" /* 16:9 */,
+          paddingTop: 25,
+          height: 0,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <iframe
+          style={{
+            position: "absolute",
+            display: "flex",
+            justifyContent: "center",
+            width: "70%",
+            height: "70%",
+          }}
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          frameBorder="0"
+        />
+      </div>
     </div>
   );
 };
