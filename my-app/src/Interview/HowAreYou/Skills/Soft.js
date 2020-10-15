9c10e64c361623b73f28ee07bd165052d2160145
kwmorlock <kmorlockcodes@gmail.com>
2020-10-15 05:44:55 -0400

    chore: Soft.js hover added

--- my-app/src/Interview/HowAreYou/Skills/Soft.js
+++ my-app/src/Interview/HowAreYou/Skills/Soft.js
@@ -2,6 +2,12 @@ import React from "react";
 import Nav from "../../Nav/Nav";
 
 const Soft = () => {
+  function changeColor(e) {
+    e.target.style.color = "fuchsia";
+  }
+  function changeColors(e) {
+    e.target.style.color = "black";
+  }
   return (
     <div>
       <Nav />
