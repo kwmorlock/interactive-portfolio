e4737ff3c2a78222cd7c4e2278705ee3c2bcca69
kwmorlock <kmorlockcodes@gmail.com>
2020-10-15 05:42:42 -0400

    chore: Soft.js Nav added

--- my-app/src/Interview/HowAreYou/Skills/Soft.js
+++ my-app/src/Interview/HowAreYou/Skills/Soft.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Nav from "../../Nav/Nav";
 
 const Soft = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
 };
 
 export default Soft;
