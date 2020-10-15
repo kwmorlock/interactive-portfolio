c7e8b2db1c721e27a5ce16189a0f363517c1a92f
kwmorlock <kmorlockcodes@gmail.com>
2020-10-15 05:46:47 -0400

    chore: Soft.js second Link

--- my-app/src/Interview/HowAreYou/Skills/Soft.js
+++ my-app/src/Interview/HowAreYou/Skills/Soft.js
@@ -27,6 +27,22 @@ const Soft = () => {
           {" "}
           Option 1{" "}
         </Link>
+        <Link
+          to={"/"}
+          style={{
+            color: "black",
+            fontColor: "black",
+            width: "65%",
+            borderRadius: "12px",
+            fontSize: "2rem",
+            textDecoration: "none",
+          }}
+          onMouseOver={changeColor}
+          onMouseLeave={changeColors}
+        >
+          {" "}
+          Option 2{" "}
+        </Link>
       <div
         className="video"
         style={{
