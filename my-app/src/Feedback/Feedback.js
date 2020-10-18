72252857c241c25db16e36e101667a8e89aa914c
kwmorlock <kmorlockcodes@gmail.com>
2020-10-18 00:34:11 -0400

    chore: Feedback.js Feedback div structure

--- my-app/src/Feedback/Feedback.js
+++ my-app/src/Feedback/Feedback.js
@@ -6,9 +6,22 @@ const Feedback = () => {
   return (
     <HomeWrapper>
       <Nav />
-      <h2 class="mysite"> Mentors</h2>
-      <h2 class="mysite"> Mentees</h2>
-      <h2 class="mysite"> Peers</h2>
+      <div>
+        <h1>Feedback</h1>
+        <p>
+          Feedback is very important to me, whether that feedback is from those
+          I'm working under, those that are working under me, or my peers.
+        </p>
+      </div>
+      <div>
+        <h2 class="mysite"> Mentors</h2>
+      </div>
+      <div>
+        <h2 class="mysite"> Mentees</h2>
+      </div>
+      <div>
+        <h2 class="mysite"> Peers</h2>
+      </div>
     </HomeWrapper>
   );
 };
