b99b49e7952f12b52382f9f6dc9aa8c470af918b
kwmorlock <kmorlockcodes@gmail.com>
2020-11-06 09:07:54 -0500

    upd: Sorry.js InterviewContents added

--- src/Interview/HowAreYou/NothingTwo/Sorry/Sorry.js
+++ src/Interview/HowAreYou/NothingTwo/Sorry/Sorry.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "../../../../Nav/Nav";
+import { InterviewContents } from "../../../InterviewStyles";
 
 const Sorry = () => {
   return (
     <div>
       <Nav />
+      <InterviewContents></InterviewContents>
     </div>
   );
 };
