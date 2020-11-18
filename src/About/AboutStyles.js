614e112a433d365334a4d01d92d916591bbef9b0
kwmorlock <kmorlockcodes@gmail.com>
2020-11-18 09:57:41 -0500

    upd: AboutStyles.js flexbox

--- src/About/AboutStyles.js
+++ src/About/AboutStyles.js
@@ -1,1 +1,29 @@
 import styled from "styled-components";
+
+export const AboutLeft = styled.div`
+  width: 48%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 4% 7px;
+`;
+
+export const FlexAbout = styled.div`
+  display: flex;
+  flex-direction: row;
+  @media (max-width: 765px) {
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const FlexAboutTwo = styled.div`
+  display: flex;
+  flex-direction: row;
+  @media (max-width: 765px) {
+    width: 100%;
+    flex-direction: column-reverse;
+    align-items: center;
+  }
+`;
