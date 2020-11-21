4c509ba6923af75e5f68cbc92d9ca3ffd656a251
kwmorlock <kmorlockcodes@gmail.com>
2020-11-21 00:47:24 -0500

    upd: About.js intro

--- src/About/About.js
+++ src/About/About.js
@@ -7,6 +7,15 @@ const About = () => {
       <Nav />
       <div>
         <h1>About Me</h1>
+        <p>
+          When I was child I asked so many questions wanting to understand how
+          everything worked a teacher once jokingly said to me “Pretend a
+          problem is a wall, and you can climb over it, go under it, or run
+          around it; you climb over it, then run around it, only to finally go
+          under it.”. After many years that curiosity I had as a child is still
+          alive more so than ever and it is one of my driving forces in my
+          journey as a full stack developer.
+        </p>
       </div>
     </div>
   );
