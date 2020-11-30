a50ba345a01e362976e1607195ad1f3e15227c93
kwmorlock <kmorlockcodes@gmail.com>
2020-11-30 01:58:49 -0500

    Feedback.js peer feedback1

--- src/Feedback/Feedback.js
+++ src/Feedback/Feedback.js
@@ -7,8 +7,8 @@ const Feedback = () => {
     <HomeWrapper>
       <Nav />
       <div>
-        <h1 style={{textAlign:'center'}}>Feedback</h1>
-        <p style={{textAlign:'center', margin: '40px'}}>
+        <h1 style={{ textAlign: "center" }}>Feedback</h1>
+        <p style={{ textAlign: "center", margin: "40px" }}>
           Feedback is very important to me, whether that feedback is from those
           I'm working under, those that are working under me, or my peers.
         </p>
@@ -21,6 +21,14 @@ const Feedback = () => {
       </div>
       <div>
         <h2 class="mysite"> Peers</h2>
+        <p>
+          Julia V. : Kenzie is a really great leader for our group, and helps to
+          lead the team on what decisions and actions we need to do for our
+          project. She also does a really amazing job at communicating with the
+          other ds team who is not within our group, asking questions and giving
+          expectations on what we what from the ds team. Kenzie is also good at
+          creating a team culture atmosphere.
+        </p>
       </div>
     </HomeWrapper>
   );
